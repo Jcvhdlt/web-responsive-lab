365d5af1920824ca0709507f4eac6e9457e64098
javier <fjzamora93@gmail.com>
2024-04-22 18:45:52 -0400

    Actualización general -Javier

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//Nos aseguramos de que todos los elementos estén cargados en la página para javascript
+
+document.addEventListener('DOMContentLoaded', function(){
+
+    //Imprimimos por consola el ancho del navegador para las pruebas
+    const anchoNavegador = window.innerWidth;
+    console.log("Ancho del navegador:", anchoNavegador);
+
+
+})
